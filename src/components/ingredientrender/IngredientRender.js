1532b46b218149367cee9381015fd436e7d72c1f
Nicolas Martinez <fyedream@gmail.com>
2019-07-21 07:46:05 -0600

    Added the burger UI component,now the app can show the ingredients

--- src/components/ingredientrender/IngredientRender.js
+++ src/components/ingredientrender/IngredientRender.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-
 import './ingredient-render.css';
 
 const IngredientRender = (props) => {
 
-    const renderElement = (ingredient) => {
-        return<div className={ingredient.cssClass}></div>
-    };
-
     return (
         <div className="ingredient-render-container">  
-            {renderElement(props.ingredient)}
+           <div className={props.ingredient.cssClass}></div>
         </div>
     );
 };
